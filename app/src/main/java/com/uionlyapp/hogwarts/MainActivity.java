1f9f447ff667cd4b71cc163012deb9439d2cb50d
Rodrigo de Oliveira <rodrigodeoliveira397@gmail.com>
2018-09-01 21:55:04 -0300

    Added a Hogwarts mail link; Changed a LinearLayout with a ImageView + TextView for a TextView + android:drawable attribute; Added support for vector drawables in API lower than 21; Adjusted TextViews font style, size and padding.

--- app/src/main/java/com/uionlyapp/hogwarts/MainActivity.java
+++ app/src/main/java/com/uionlyapp/hogwarts/MainActivity.java
@@ -2,6 +2,8 @@ package com.uionlyapp.hogwarts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView mail = findViewById(R.id.mail_link);
+        mail.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
